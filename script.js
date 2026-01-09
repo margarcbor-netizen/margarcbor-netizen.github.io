b0522b7a9c320595bb1070c3d967e6852138160e
margarcbor <margarcbor@gmail.com>
2026-01-09 14:57:16 +0100

    efecto blurry guay, pero incompatible con home actual

--- script.js
+++ script.js
@@ -2,17 +2,41 @@
 
 const archivoBtn = document.getElementById('archivoBtn');
 const archivoPanel = document.getElementById('archivoPanel');
+const flowTrack = document.getElementById("flowTrack");
 
 archivoBtn.addEventListener('click', () => {
-    archivoPanel.classList.toggle('open');
+    archivoPanel.classList.toggle('desplegar');
     
-    /*vinculado a cambio de color boton al abrir panel
-    archivoPanel.classList.toggle("is-open"); 
-    document.body.classList.toggle("archivo-open");*/
 });
 
 archivoPanel.querySelectorAll('a').forEach(link => {
   link.addEventListener('click', () => {
-    archivoPanel.classList.remove('open');
+    archivoPanel.classList.remove('desplegar');
   });
 })
+
+
+
+/*al desplegar panel ocultar carroulsel = funciona, pero el carrousel ensucia. quiza si
+home es de otra forma puedes usarlo u mola
+
+archivoBtn.addEventListener('click', () => {
+    archivoPanel.classList.toggle('desplegar');
+
+      flowTrack.classList.toggle(
+      "oculto",
+      archivoPanel.classList.contains("desplegar")
+  );
+    
+});
+
+boton.addEventListener("click", () => {
+  panel.classList.toggle("desplegar");
+
+  if (panel.classList.contains("desplegar")) {
+    carrusel.classList.add("ocultar");
+  } else {
+    carrusel.classList.remove("ocultar");
+  }
+});*/
+
